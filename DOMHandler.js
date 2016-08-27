e21804440c3cfc1c43b1598f8db534b940e421a5
jmarcusgill <jmarcusgill@gmail.com>
2016-08-27 13:42:37 -0500

    added DOM and index

--- DOMHandler.js
+++ DOMHandler.js
@@ -5,6 +5,7 @@ var languageChooser = document.getElementById("translate");
 languageChooser.addEventListener("click",function(event){
 	selecetedLanguage = document.getElementById("language").value;
 	console.log(selecetedLanguage);
+  console.log(english)
 });
 
 function output(outputLanguage){
